dd5590f286f772e8274bcd01056745efa7c3aabd
Sharu24 <sharu.mukti@gmail.com>
2020-11-27 22:14:14 +0530

    added

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const bcrypt = require("bcrypt");
+const { body, validationResult } = require("express-validator");
 const app = express();
 
 require("./dbConnect");
@@ -13,16 +15,26 @@ app.get("/", (req, res) => {
   res.send("Home Get Route");
 });
 
-app.post("/", async (req, res) => {
-  try {
-    const userData = new User(req.body);
-    await userData.save();
-    res.status(200).json({ Success: "User Created Successfully" });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ Error: "failed to create" });
+app.post(
+  "/",
+  [
+    body("firstName", "First Name is required").notEmpty(),
+    body("lastName", "Last Name is required").notEmpty(),
+    body("password", "Password is required").notEmpty()
+  ],
+  async (req, res) => {
+    try {
+      const userData = new User(req.body);
+      const saltRounds = 10;
+      userData.password = bcrypt.hashSync(req.body.password, saltRounds);
+      await userData.save();
+      res.status(200).json({ Success: "User Created Successfully" });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({ Error: "failed to create" });
+    }
   }
-});
+);
 
 app.listen(port, () => {
   console.log("Server is listening on port " + port);
--- dbConnect.js
+++ dbConnect.js
@@ -1,17 +1,9 @@
 const mongoose = require("mongoose");
-
-const options = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-};
-
-const mongoUri = "mongodb://localhost/reactdb";
-const mongoCloud =
-  "mongodb+srv://sharu24:Dominar@24@bootcamp.i5p6q.gcp.mongodb.net/reactdb?retryWrites=true&w=majority";
+const config = require("./config");
 
 const dbConnect = async () => {
   try {
-    await mongoose.connect(mongoUri, options);
+    await mongoose.connect(config.mongoCloud, config.options);
     console.log("Successfully connected to mongodb");
   } catch (err) {
     console.log("Unable to connect to mongo db");
